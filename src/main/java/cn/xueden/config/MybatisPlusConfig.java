24274ab5f2e3dc77d67791c37613109165110eb2
吴雪伟 <mogv@com>
2023-05-14 00:21:08 +0800

    task：房间类型接口交付

---
+++ src/main/java/cn/xueden/config/MybatisPlusConfig.java
@@ -0,0 +1,26 @@
+package cn.xueden.config;
+
+import com.baomidou.mybatisplus.core.config.GlobalConfig;
+import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.OptimisticLockerInnerInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@MapperScan(basePackages = {"cn.xueden.**.mapper"})
+public class MybatisPlusConfig {
+    @Bean
+    public MybatisPlusInterceptor mybatisPlusInterceptor() {
+        MybatisPlusInterceptor interceptor = new MybatisPlusInterceptor();
+        interceptor.addInnerInterceptor(new OptimisticLockerInnerInterceptor());   //    注册乐观锁插件
+        interceptor.addInnerInterceptor(new PaginationInnerInterceptor());    // 分页插件
+        return interceptor;
+    }
+
+
+
+}
+
+
--- src/main/java/cn/xueden/hotel/domain/controller/HotelRoomTypeController.java
+++ src/main/java/cn/xueden/hotel/domain/controller/HotelRoomTypeController.java
@@ -1,18 +1,22 @@
 package cn.xueden.hotel.domain.controller;
 
+import cn.hutool.json.JSONObject;
 import cn.xueden.common.api.Result;
 import cn.xueden.hotel.domain.entity.HotelRoomType;
 import cn.xueden.hotel.domain.service.IHotelRoomTypeService;
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.baomidou.mybatisplus.core.metadata.IPage;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.github.houbb.auto.log.annotation.AutoLog;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpServletRequest;
 
 /**
  * 名称:HotelRoomTypeController
@@ -28,6 +32,8 @@ public class HotelRoomTypeController {
 
     @Autowired
     private IHotelRoomTypeService hotelRoomTypeService;
+    private Object QueryGenerator;
+
     /**
      *   添加
      *
@@ -38,8 +44,82 @@ public class HotelRoomTypeController {
     @ApiOperation(value="房间类型-添加", notes="房间类型-添加")
     @PostMapping("/add")
     public Result<?> add(@RequestBody HotelRoomType hotelRoomType){
-       hotelRoomTypeService.save(hotelRoomType);
-       return  Result.ok("添加成功！");
+        QueryWrapper<HotelRoomType> queryWrapper=new QueryWrapper<>();
+        queryWrapper.lambda().eq(HotelRoomType::getTypeSort,hotelRoomType.getTypeSort());
+        if(StringUtils.isNotEmpty(hotelRoomType.getId().toString())&&hotelRoomType.getId()!=0){ //更新逻辑
+            queryWrapper.lambda().ne(HotelRoomType::getId,hotelRoomType.getId());
+            int  count= (int) hotelRoomTypeService.count(queryWrapper);
+            if(count>=1){
+                return  Result.error("类型序号已存在");
+            }
+            hotelRoomTypeService.saveOrUpdate(hotelRoomType);
+            return  Result.ok("更新成功！");
+        }else{ //新增逻辑
+            int  count= (int) hotelRoomTypeService.count(queryWrapper);
+            if(count>=1){
+                return  Result.error("类型序号已存在");
+            }
+            hotelRoomTypeService.save(hotelRoomType);
+            return  Result.ok("添加成功！");
+        }
+    }
+    /**
+     *   查询
+     *
+     * @param hotelRoomType
+     * @return
+     */
+    @AutoLog(description = "房间类型-查询")
+    @ApiOperation(value="房间类型-查询", notes="房间类型-查询")
+    @GetMapping("/list")
+    public Result<?> queryPageList(HotelRoomType hotelRoomType,
+                                   @RequestParam(name="pageNo",defaultValue="1") Integer pageNo,
+                                   @RequestParam(name="pageSize",defaultValue="10") Integer pageSize,
+                                   @RequestParam(name="typeName",required = false) String typeName
+                                   ){ //HttpServletRequest req
+        QueryWrapper<HotelRoomType> queryWrapper= new QueryWrapper();
+        if(StringUtils.isNotEmpty(typeName)){
+            queryWrapper.lambda().like(HotelRoomType::getTypeName,typeName);
+        }
+        Page<HotelRoomType> page=new Page<>(pageNo,pageSize);
+        IPage<HotelRoomType> pageList=  hotelRoomTypeService.page(page,queryWrapper);
+        return Result.ok(pageList);
+
+    }
+    /**
+     *   查询
+     *
+     * @param id
+     * @return
+     */
+    @AutoLog(description = "房间类型详情-查询")
+    @ApiOperation(value="房间类型详情-查询", notes="房间类型详情-查询")
+    @GetMapping("/info")
+    public Result<?> info(@RequestParam(name="id",required=true) int id){
+        QueryWrapper<HotelRoomType> queryWrapper=new QueryWrapper<>();
+//        queryWrapper.lambda().eq(HotelRoomType::getId,id);
+        HotelRoomType hotelRoomType= hotelRoomTypeService.getById(id);
+        return Result.ok(hotelRoomType);
+    }
+    /**
+     *   删除
+     * @param id
+     * @return
+     */
+    @AutoLog(description = "房间类型-删除")
+    @ApiOperation(value="房间类型-删除", notes="房间类型-删除")
+    @PostMapping("/delete")
+    public Result<?> delete(@RequestBody JSONObject jsonObject){
+        if (!jsonObject.containsKey("id")||!StringUtils.isNotBlank(jsonObject.getStr("id"))) {
+            return Result.error("id不能为空！");
+        }
+        if (StringUtils.isNotBlank(jsonObject.getStr("id"))){
+            hotelRoomTypeService.removeById(jsonObject.getStr("id"));
+        }
+//        QueryWrapper<HotelRoomType> queryWrapper=new QueryWrapper<>();
+//        queryWrapper.lambda().eq(HotelRoomType::getId,jsonObject.containsKey("id"));
+//        hotelRoomTypeService.remove(queryWrapper);
+        return Result.ok("删除成功！");
     }
 
 
